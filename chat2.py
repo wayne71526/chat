e9a1952edacb3a0e775af0d96235f359b981480a
Wayne <a0918964526@gmail.com>
2020-09-15 17:34:39 +0800

    add chat2.py

---
+++ chat2.py
@@ -0,0 +1,12 @@
+# 清單的分割可以用在字串
+lines = []
+with open('3.txt', 'r', encoding='utf-8-sig') as f:
+	for line in f:
+		lines.append(line.strip())
+
+for line in lines:
+	s = line.split(' ')
+	time = s[0][:5]
+	name = s[0][5:]
+	print(time)
+	print(name)		
